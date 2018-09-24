2c347b2257fa29f04522f2cf4c1488a175b0ebbe
21867 <43053741+21867@users.noreply.github.com>
2018-09-23 17:27:59 -0700

    Add files via upload

---
+++ GroceryPickerUpper.java
@@ -0,0 +1,53 @@
+/**
+   @Author:
+      Date: Sep 07, 2006
+   Teacher:
+       Lab:
+      Misc:
+  */
+
+import kareltherobot.*;
+
+public class GroceryPickerUpper implements Directions
+{
+    public static void task(UrRobot karel)  {
+       karel.turnLeft();
+       karel.turnLeft();
+       karel.move();
+       karel.move();
+       karel.pickBeeper();
+       karel.move();
+       karel.turnLeft();
+       karel.turnLeft();
+       karel.turnLeft();
+       karel.move();
+       karel.pickBeeper();
+       karel.move();
+       karel.turnLeft();
+       karel.move();
+       karel.pickBeeper();
+       karel.turnLeft();
+       karel.turnLeft();
+       karel.turnLeft();
+       karel.move();
+       karel.pickBeeper();
+       karel.turnLeft();
+       karel.turnLeft();
+       karel.move();
+       karel.move();
+       karel.move();
+       karel.turnLeft();
+       karel.move();
+       karel.move();
+       karel.move();
+       karel.move();
+       karel.turnOff();
+       
+
+    //add your code here
+        
+       
+       
+    } 
+
+}
